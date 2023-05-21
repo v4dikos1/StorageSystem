582e3bc1907e42082f5c81d30e144b2a1355a7ca
v4d1kos1 <v4dikos@yandex.ru>
2023-05-21 20:36:10 +0700

    persistence dependency injection configured

---
+++ Persistence/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DbConnection");
+        services.AddDbContext<StorageServiceDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+        services.AddScoped<IApplicationDbContext>(provider => provider.GetService<StorageServiceDbContext>()!);
+
+        return services;
+    }
+}
