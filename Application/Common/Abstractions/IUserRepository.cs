c7d3ed5a5d0a9eeb3132e3130d536f4a560a9f82
v4d1kos1 <v4dikos@yandex.ru>
2023-05-21 19:53:16 +0700

    Registration command is implemented, also added password hashing service interface

--- Application/Common/Abstractions/IUserRepository.cs
+++ Application/Common/Abstractions/IUserRepository.cs
@@ -1,21 +1,20 @@
 ﻿using Application.Models;
-using Domain.ValueObjects;
 
 namespace Application.Common.Abstractions;
 
 public interface IUserRepository
 {
-    public User CreateUser(Username username, Email email, byte[] passwordHash, byte[] passwordSalt, bool isEmailConfirmed);
+    public User CreateUser(string username, string email, byte[] passwordHash, byte[] passwordSalt, bool isEmailConfirmed);
 
     public User UpdateUsername(Guid userId, string username);
     public User UpdateEmail(Guid userId, string email);
     public User UpdatePassword(Guid userId, byte[] passwordHash, byte[] passwordSalt);
-    public User UpdateUser(Guid userId, Username username, Email email, byte[] passwordHash, byte[] passwordSalt, bool isEmailConfirmed);
+    public User UpdateUser(Guid userId, string username, string email, byte[] passwordHash, byte[] passwordSalt, bool isEmailConfirmed);
     public User ConfirmEmail (Guid userId);
 
     public bool DeleteUser (Guid userId);
 
     public Task<List<User>> GetUsersAsync(int offset, int limit, CancellationToken cancellationToken);
-    public Task<User> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken);
-    public Task<User> GetUserByEmailAsync(Email email, CancellationToken cancellationToken);
+    public Task<User?> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken);
+    public Task<User?> GetUserByEmailAsync(string email, CancellationToken cancellationToken);
 }
--- Application/Users/Commands/Registration/RegistrationCommandHandler.cs
+++ Application/Users/Commands/Registration/RegistrationCommandHandler.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Common.Abstractions;
+using Application.Common.Exceptions;
+using Application.Models;
 using MediatR;
 
 namespace Application.Users.Commands.Registration;
@@ -7,15 +9,35 @@ public class RegistrationCommandHandler : IRequestHandler<RegistrationCommand, G
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IUserRepository _userRepository;
+    private readonly IPasswordService _passwordService;
 
-    public RegistrationCommandHandler(IUnitOfWork unitOfWork, IUserRepository userRepository)
+    public RegistrationCommandHandler(IUnitOfWork unitOfWork, IUserRepository userRepository, IPasswordService passwordService)
     {
         _unitOfWork = unitOfWork;
         _userRepository = userRepository;
+        _passwordService = passwordService;
     }
 
     public async Task<Guid> Handle(RegistrationCommand request, CancellationToken cancellationToken)
     {
-        
+        // Check if a user with the same email already exists
+        if (await _userRepository.GetUserByEmailAsync(request.Email.Value, cancellationToken) is not null)
+        {
+            throw new AlreadyExistsException(nameof(User), request.Email.Value);
+        }
+
+        // Hashing the password
+        _passwordService.CreatePasswordHash(request.Password, out var passwordHash, out var passwordSalt);
+
+        var user = _userRepository.CreateUser(
+            request.Username.Value,
+            request.Email.Value,
+            passwordHash,
+            passwordSalt,
+            false);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return user.Id;
     }
 }
