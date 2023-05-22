6207c59d839bfd8a10125cdafbd9185d57cd43a4
v4d1kos1 <v4dikos@yandex.ru>
2023-05-22 18:47:09 +0700

    Setting up a project, options to run.

---
+++ StorageSystemApi/OptionsSetup/ConfigureSwaggerOptions.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
+namespace StorageSystemApi.OptionsSetup;
+
+/// <summary>
+/// Configuring options for swagger
+/// </summary>
+public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider _provider;
+
+    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+    {
+        _provider = provider;
+    }
+
+    public void Configure(SwaggerGenOptions options)
+    {
+        foreach (var description in _provider.ApiVersionDescriptions)
+        {
+            var apiVersion = description.ApiVersion.ToString();
+            options.SwaggerDoc(description.GroupName,
+                new OpenApiInfo
+                {
+                    Version = apiVersion,
+                    Title = $"StorageSystem {apiVersion}",
+                    Description = "StorageSystem API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Vadim",
+                        Email = "v4dikos@yandex.ru",
+                        Url = new Uri("https://t.me/v4dikos")
+                    }
+                });
+
+            options.AddSecurityDefinition($"AuthToken {apiVersion}",
+                new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer",
+                    Name = "Authorization",
+                    Description = "Authorization token"
+                });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                             Reference = new OpenApiReference
+                             {
+                                 Type = ReferenceType.SecurityScheme,
+                                 Id = $"AuthToken {apiVersion}",
+                             }
+                        },
+                             new string[] {}
+                    }
+                });
+
+            options.CustomOperationIds(apiDescription =>
+                apiDescription.TryGetMethodInfo(out MethodInfo methodInfo)
+                                                ? methodInfo.Name
+                                                : null
+                                       );
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            options.IncludeXmlComments(xmlPath);
+        }
+    }
+}
