4958a5676e19a970207bcbbce973d66e65ddce50
v4d1kos1 <v4dikos@yandex.ru>
2023-05-22 18:45:47 +0700

    Implemented middleware to handle custom exeptions

---
+++ StorageSystemApi/Middleware/CustomExceptionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace StorageSystemApi.Middleware;
+
+public static class CustomExceptionHandlerMiddlewareExtensions
+{
+    /// <summary>
+    /// An extension method for applying the custom intermediate level.
+    /// </summary>
+    public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder applicationBuilder)
+    {
+        return applicationBuilder.UseMiddleware<CustomExceptionHandlerMiddleware>();
+    }
+}
